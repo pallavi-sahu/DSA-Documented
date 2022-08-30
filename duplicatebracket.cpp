9a25e3c311494853a0fb860da6f13665739284e6
pallavi-sahu <pallavisahu0610@gmail.com>
2022-08-30 17:46:25 +0530

    extra bracket

--- duplicatebracket.cpp
+++ duplicatebracket.cpp
@@ -1,13 +1,27 @@
 #include<bits/stdc++.h>
 #include<algorithm>
 #include<string>
+#include<stack>
+using namespace std;
 
-stack<char> str;
+stack<char> stk;
 bool duplicatebracket(string s){
-     
+     for(int i=0; i<s.size(); i++){
+        if(s[i]==')'){
+             if(stk.top()=='(') return true;
+             else {
+                while(stk.top()!='(') 
+                     stk.pop();
+            }
+            stk.pop();
+        }
+        else stk.push(s[i]);
+     }
+     return false;
 }
 
  int main(){
-     
+     duplicatebracket("((a+b)+(c+d))");
+     return 0;
 
  }
