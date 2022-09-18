7ee0e01427f70c96321be76f5bc135c8f73d29a3
pallavi-sahu <pallavisahu0610@gmail.com>
2022-09-18 10:02:55 +0530

    stack

--- duplicatebracket.cpp
+++ duplicatebracket.cpp
@@ -2,26 +2,29 @@
 #include<algorithm>
 #include<string>
 #include<stack>
+#include<algorithm>
 using namespace std;
 
 stack<char> stk;
-bool duplicatebracket(string s){
+bool balancedbracket(string s){
      for(int i=0; i<s.size(); i++){
-        if(s[i]==')'){
-             if(stk.top()=='(') return true;
-             else {
-                while(stk.top()!='(') 
-                     stk.pop();
-            }
-            stk.pop();
+        if(stk.empty())
+        stk.push(s[i]);
+        else{
+            if(stk.top()=='('&&s[i]==')' || stk.top()=='{'&&s[i]=='}' || stk.top()=='['&&s[i]==']') stk.pop();
+            else stk.push(s[i]);
         }
-        else stk.push(s[i]);
      }
-     return false;
+    
+    
+    if(stk.empty()) return true; 
+    else return false;
 }
 
- int main(){
-     duplicatebracket("((a+b)+(c+d))");
-     return 0;
 
- }
+int main(){
+    balancedbracket("((())");
+    if(stk.empty()) cout<<"true"; 
+    else cout<< "false";
+    return 0;
+}
